b075c55809fd0ff90aec7e456a9025bdd5a26493
NightmareVCO <vladimircuriel@outlook.com>
2025-07-01 17:16:29 -0400

    feat: add EnumType annotation to category field in Product entity

--- server/src/main/java/inventory/management/qa/server/entities/Product.java
+++ server/src/main/java/inventory/management/qa/server/entities/Product.java
@@ -1,6 +1,8 @@
 package inventory.management.qa.server.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,6 +15,7 @@ public class Product {
 
   private String name;
   private String description;
+  @Enumerated(EnumType.STRING)
   private Category category;
   private double price;
   private int quantity;
